0e4f1798295ea074fd02915b8ab62ef31edc4d3d
Kellon <sil3ntfn@gmail.com>
2021-10-06 11:07:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ bot.on('message', async (message) => {
 const supportGuild = bot.guilds.cache.get('779043676214263808')
 const member = supportGuild.members.cache.get(message.author.id)
 
-
+//i hate you
 const role1 = member ? member.roles.cache.get('810838470200131605') : false
 const role2 = member ? member.roles.cache.get('810838438173343745') : false
 const role3 = member ? member.roles.cache.get('678114167902830610') : false
